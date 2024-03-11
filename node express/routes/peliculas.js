f16f3208ea48384a7dad1e779a0697afb89a5eea
akuh2o <facundoballestero@hotmail.com>
2024-03-11 16:09:49 -0300

    tarea de express

---
+++ node express/routes/peliculas.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.send('Pagina de peliculas') //es con send
+});
+
+module.exports = router;
